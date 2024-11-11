0b16974d1a7b92f3a0b137d578cd9d11c318f872
Mathis Randl <mathis.randl@epfl.ch>
2024-11-11 11:15:07 +0100

    testing invariant for vec distance

--- src/linalg/vector.rs
+++ src/linalg/vector.rs
@@ -1,10 +1,10 @@
-pub struct Vector<'a> {
-    _repr: &'a [f32],
+pub struct F32Vector<'a> {
+    array: &'a [f32],
 }
 
-impl<'a> Vector<'a> {
+impl<'a> F32Vector<'a> {
     pub fn len(&self) -> usize {
-        self._repr.len()
+        self.array.len()
     }
 
     /// # Usage
@@ -19,12 +19,12 @@ impl<'a> Vector<'a> {
     /// Panics in debug mode if the two vectors have different lengths.
     /// In release mode, the longest vector will be silently truncated.
     #[inline]
-    pub fn l2_dist_squared(&self, other: &Vector<'a>) -> f32 {
+    pub fn l2_dist_squared(&self, other: &F32Vector<'a>) -> f32 {
         debug_assert!(self.len() == other.len());
 
-        self._repr
+        self.array
             .iter()
-            .zip(other._repr)
+            .zip(other.array)
             .map(|(x, y)| {
                 let diff = x - y;
                 diff * diff
@@ -40,14 +40,14 @@ impl<'a> Vector<'a> {
     /// Panics in debug mode if the two vectors have different lengths.
     /// In release mode, the longest vector will be silently truncated.
     #[inline]
-    pub fn l2_dist(&self, other: &Vector<'a>) -> f32 {
+    pub fn l2_dist(&self, other: &F32Vector<'a>) -> f32 {
         self.l2_dist_squared(other).sqrt()
     }
 }
 
-impl<'a> From<&'a [f32]> for Vector<'a> {
+impl<'a> From<&'a [f32]> for F32Vector<'a> {
     fn from(value: &'a [f32]) -> Self {
-        Vector { _repr: value }
+        F32Vector { array: value }
     }
 }
 
@@ -72,7 +72,7 @@ mod tests {
             if totest.iter().any(|x| !x.is_finite()) {
                 return TestResult::discard();
             }
-            let testvec = Vector::from(&totest[..]);
+            let testvec = F32Vector::from(&totest[..]);
             let selfsim = testvec.l2_dist(&testvec);
             let to_check = is_valid_l2(selfsim) && close(selfsim, 0.0);
             return TestResult::from_bool(to_check);
@@ -84,5 +84,33 @@ mod tests {
             // i.e. at least 10% of inputs should be valid so that we cover a good range
             .min_tests_passed(1_000)
             .quickcheck(qc_self_sim_is_zero as fn(Vec<f32>) -> TestResult);
+    }
+
+    #[test]
+    // verifies the claim in the documentation of l2_dist_squared
+    // i.e. dist(u,v) < dist(w, x) ⇔ dist(u,v) ** 2 < dist(w,x) ** 2
+    fn squared_invariant() {
+        fn qc_squared_invariant(u: Vec<f32>, v: Vec<f32>, w: Vec<f32>, x: Vec<f32>) -> TestResult {
+            let all_vecs = [u, v, w, x]; //no need to check for NaNs in this case
+            let min_length = all_vecs.iter().map(|x| x.len()).min().unwrap();
+            let all_vectors: Vec<F32Vector> = all_vecs
+                .iter()
+                .map(|vec| F32Vector::from(&vec[..min_length]))
+                .collect();
+
+            let d1_squared = all_vectors[0].l2_dist_squared(&all_vectors[1]);
+            let d2_squared = all_vectors[2].l2_dist_squared(&all_vectors[3]);
+
+            let d1_root = all_vectors[0].l2_dist(&all_vectors[1]);
+            let d2_root = all_vectors[2].l2_dist(&all_vectors[3]);
+
+            let sanity_check1 = (d1_squared < d2_squared) == (d1_root < d2_root);
+            let sanity_check2 = (d1_squared <= d2_squared) == (d1_root <= d2_root);
+            TestResult::from_bool(sanity_check1 && sanity_check2)
+        }
+
+        QuickCheck::new().tests(10_000).quickcheck(
+            qc_squared_invariant as fn(Vec<f32>, Vec<f32>, Vec<f32>, Vec<f32>) -> TestResult,
+        );
     }
 }
