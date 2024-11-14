ca4edcb327bf40027ea40504f7c0858028984700
Mathis Randl <mathis.randl@epfl.ch>
2024-11-14 20:00:32 +0100

    randomized benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![allow(dead_code)]
+
+extern crate rand;
+
 mod caching;
 mod numerics;
 
@@ -9,17 +12,27 @@ fn main() {
 mod tests {
     use std::time::Instant;
 
+    use rand::Rng as _;
+
     use crate::numerics::f32vector::F32Vector;
 
     const VEC_SIZE: usize = (u32::MAX / 128) as usize;
     #[test]
     fn perftest() {
-        let data: Vec<_> = (0..(u32::MAX / 128)).map(|x| f32::from(x as u16)).collect();
-        let vectorized = F32Vector::from(&data[..]);
+        let mut rng = rand::thread_rng();
+        let v1: Vec<_> = (0..128)
+            .map(|_| f32::from(rng.gen_range(-20 as i16..20)))
+            .collect();
+        let v2: Vec<_> = (0..128)
+            .map(|_| f32::from(rng.gen_range(-20 as i16..20)))
+            .collect();
+
+        let v1_f32v = F32Vector::from(&v1[..]);
+        let v2_f32v = F32Vector::from(&v2[..]);
         let start_t = Instant::now();
         let mut sum = 0.0;
-        for _ in 0..200 {
-            sum += vectorized.l2_dist(&vectorized);
+        for _ in 0..10_000_000 {
+            sum += v1_f32v.l2_dist(&v2_f32v);
         }
         let elapsed = start_t.elapsed();
         println!("{:?} - {sum}", elapsed)
