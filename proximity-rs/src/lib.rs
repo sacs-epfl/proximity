45d477ac5eb19e0293461eca12fbc304ef9ddd10
Mathis Randl <mathis.randl@epfl.ch>
2025-01-08 14:11:12 +0100

    cargo fmt

--- proximity-rs/src/lib.rs
+++ proximity-rs/src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(portable_simd, test, array_chunks)]
 
-
 extern crate npyz;
 extern crate rand;
 extern crate test;
@@ -8,5 +7,3 @@ extern crate test;
 pub mod caching;
 pub mod fs;
 pub mod numerics;
-
-
