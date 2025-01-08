8772e557d20b7f4233402e8a7cbf9e66939864ca
Mathis Randl <mathis.randl@epfl.ch>
2025-01-08 14:09:19 +0100

    attempt at new CI deployment #1/?

--- proximity-rs/src/caching/bounded/bounded_linear_cache.rs
+++ proximity-rs/src/caching/bounded/bounded_linear_cache.rs
@@ -1,8 +1,6 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-use pyo3::{pyclass, pymethods};
-
 use crate::numerics::comp::ApproxComparable;
 
 use crate::caching::approximate_cache::ApproximateCache;
@@ -20,8 +18,8 @@ use super::list_node::{Node, SharedNode};
 ///
 /// # Example Usage
 /// ```
-/// use proximitylib::caching::bounded::bounded_linear_cache::BoundedLinearCache;
-/// use proximitylib::caching::approximate_cache::ApproximateCache;
+/// use proximipy::caching::bounded::bounded_linear_cache::BoundedLinearCache;
+/// use proximipy::caching::approximate_cache::ApproximateCache;
 ///
 /// let mut cache = BoundedLinearCache::new(3, 2.0);
 ///
@@ -96,48 +94,6 @@ impl<K, V> BoundedLinearCache<K, V> {
         }
     }
 }
-
-macro_rules! create_pythonized_interface {
-    ($name: ident, $type: ident) => {
-        // unsendable == should hard-crash if Python tries to access it from
-        // two different Python threads.
-        //
-        // The implementation is very much thread-unsafe anyways (lots of mutations),
-        // so this is an OK behavior, we will detect it with a nice backtrace
-        // and without UB.
-        //
-        // Even in the case where we want the cache to be multithreaded, this would
-        // happen on the Rust side and will not be visible to the Python ML pipeline.
-        #[pyclass(unsendable)]
-        pub struct $name {
-            inner: BoundedLinearCache<$type, $type>,
-        }
-
-        #[pymethods]
-        impl $name {
-            #[new]
-            pub fn new(max_capacity: usize, tolerance: f32) -> Self {
-                Self {
-                    inner: BoundedLinearCache::new(max_capacity, tolerance),
-                }
-            }
-
-            fn find(&mut self, k: $type) -> Option<$type> {
-                self.inner.find(&k)
-            }
-
-            fn insert(&mut self, key: $type, value: $type) {
-                self.inner.insert(key, value)
-            }
-
-            fn len(&self) -> usize {
-                self.inner.len()
-            }
-        }
-    };
-}
-
-create_pythonized_interface!(I16Cache, i16);
 
 #[cfg(test)]
 mod tests {
--- proximity-rs/src/lib.rs
+++ proximity-rs/src/lib.rs
@@ -1,7 +1,5 @@
 #![feature(portable_simd, test, array_chunks)]
 
-use caching::bounded::bounded_linear_cache::I16Cache;
-use pyo3::prelude::*;
 
 extern crate npyz;
 extern crate rand;
@@ -11,14 +9,4 @@ pub mod caching;
 pub mod fs;
 pub mod numerics;
 
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
-#[pymodule]
-fn proximipy(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_class::<I16Cache>()?;
-    Ok(())
-}
